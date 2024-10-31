1befb1ef7406a402df5ff5bead024aac85d89083
Edson Shimano <edson_sh30@msn.com>
2024-10-31 20:59:19 -0300

    ok

--- exercicio012/exercicio015/script.js
+++ exercicio012/exercicio015/script.js
@@ -1,11 +1,52 @@
-function verificar() {
+function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('Verifique os dados e tente novamente')
 
-    if (fano.value.length == 0 || fano.value > ano) {
-     window.alert('[ERRO] Verifique os dados e tente novamente')
-    } else {
-     window.alert('tudo ok')
+    }else {
+        var fsex=document.getElementsByName('radsex')
+        var idade= ano - Number(fano.value)
+        var gênero = ''
+        var img= document.createElement('img') 
+        img.setAttribute ('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if(idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src','criancamenino.png' )
+            }else if (idade <21){
+                //Jovem
+                img.setAttribute('src','jovemhomem.png' )
+            }else if ( idade < 50){
+                //Adulto
+                img.setAttribute('src','adultohomem.png' )
+            }else {
+                //Idoso
+                img.setAttribute('src','idosohomem.png' )
+            }
+                
+        }else if (fsex[1].checked){
+            gênero =  'Mulher'
+            if(idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src','criancamenina.png' )
+            }else if (idade <21){
+                //Jovem
+                img.setAttribute('src','jovemmulher.png' )
+            }else if ( idade < 50){
+                //Adulto
+                img.setAttribute('src','adultomulher.png' )
+            }else {
+                //Idoso
+                img.setAttribute('src','idosomulher.png' )
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
+    
+}
