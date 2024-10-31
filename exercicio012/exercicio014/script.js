08f9c771244aba6128756ba3b405af5093719f3a
Edson Shimano <edson_sh30@msn.com>
2024-10-30 21:12:26 -0300

    ok

--- exercicio012/exercicio014/script.js
+++ exercicio012/exercicio014/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+  
+    }
+    
+
+
+
