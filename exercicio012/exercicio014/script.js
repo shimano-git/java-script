f99dcf1cb61316c92af2eb734571ea6053192d92
Edson Shimano <edson_sh30@msn.com>
2024-10-30 21:48:21 -0300

    ok

--- exercicio012/exercicio014/script.js
+++ exercicio012/exercicio014/script.js
@@ -1,9 +1,23 @@
 function carregar() {
-    var msg = document.getElementById('msg')
+    var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = 'imagens/imagemmanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // boa tarde
+        img.src = 'imagens/imgemtarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // boa noite
+        img.src = 'imagens/imagemnoite.png'
+        document.body.style.background = '#515154'
+    }
   
     }
     
